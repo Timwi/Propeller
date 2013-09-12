54b0225d2d6f67658260ef192c2e1f340650085d
Timwi <github@timwi.de>
2013-09-12 10:17:18 +0100

    * Rename SimplePropellerModule to PropellerModuleBase * Comment fixes (imported from Perforce changelist 5056)

---
+++ API/PropellerModuleBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RT.Servers;
+using RT.Util;
+using RT.Util.Json;
+using RT.Util.Serialization;
+
+namespace RT.PropellerApi
+{
+    /// <summary>
+    ///     Provides a default base implementation for Propeller modules.</summary>
+    /// <typeparam name="TSettings">
+    ///     The type the module uses to represent its settings.</typeparam>
+    public abstract class PropellerModuleBase<TSettings> : IPropellerModule where TSettings : class, new()
+    {
+        /// <summary>When overridden in a derived class, returns the human-readable name of the module.</summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        ///     Gets or sets the module’s current settings.</summary>
+        /// <remarks>
+        ///     This property is automatically populated before <see cref="Init"/> is called.</remarks>
+        protected TSettings Settings { get; private set; }
+
+        void IPropellerModule.Init(LoggerBase log, JsonValue settings, ISettingsSaver saver)
+        {
+            Settings = ClassifyJson.Deserialize<TSettings>(settings) ?? new TSettings();
+            saver.SaveSettings(ClassifyJson.Serialize(Settings));
+            Init(log);
+        }
+
+        /// <summary>
+        ///     When overridden in a derived class, initializes the module. The default implementation does nothing.</summary>
+        /// <param name="log">
+        ///     A logger that can be used to log messages in the Propeller log.</param>
+        public virtual void Init(LoggerBase log) { }
+
+        /// <summary>
+        ///     When overridden in a derived class, implements <see
+        ///     cref="IPropellerModule.FileFiltersToBeMonitoredForChanges"/>. The default implementation returns <c>null</c>.</summary>
+        public virtual string[] FileFiltersToBeMonitoredForChanges { get { return null; } }
+
+        /// <summary>
+        ///     When overridden in a derived class, handles an HTTP request.</summary>
+        /// <param name="req">
+        ///     HTTP request to handle.</param>
+        /// <returns>
+        ///     HTTP response.</returns>
+        public abstract HttpResponse Handle(HttpRequest req);
+
+        /// <summary>
+        ///     When overridden in a derived class, implements <see cref="IPropellerModule.MustReinitialize"/>. The default
+        ///     implementation returns <c>false</c>.</summary>
+        public virtual bool MustReinitialize { get { return false; } }
+
+        /// <summary>
+        ///     When overridden in a derived class, implements <see cref="IPropellerModule.Shutdown"/>. The default
+        ///     implementation does nothing.</summary>
+        public virtual void Shutdown() { }
+    }
+}
--- Src/Modules/Redirector/Redirector.cs
+++ Src/Modules/Redirector/Redirector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using RT.PropellerApi;
@@ -12,12 +12,12 @@ using RT.Util.Serialization;
 [assembly: AssemblyCopyright("Copyright © CuteBits 2013")]
 [assembly: ComVisible(false)]
 [assembly: Guid("1a75d829-1b88-42ff-b1f3-c8e925c04d7b")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.9999.9999")]
+[assembly: AssemblyFileVersion("1.0.9999.9999")]
 
 namespace RT.Propeller.Modules
 {
-    public sealed class Redirector : SimplePropellerModule<RedirectorSettings>
+    public sealed class Redirector : PropellerModuleBase<RedirectorSettings>
     {
         public override string Name { get { return "Redirector"; } }
 
