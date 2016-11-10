213b86a9a653521c6926006097d95a2d1768b5c3
Timwi <github@timwi.de>
2016-11-10 08:46:14 +0100

    Make ModuleInitializationException serializable.

--- Src/ModuleInitializationException.cs
+++ Src/ModuleInitializationException.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace RT.Propeller
 {
     [Serializable]
-    sealed class ModuleInitializationException : Exception
+    class ModuleInitializationException : Exception
     {
         public ModuleInitializationException() : this(null, null) { }
         public ModuleInitializationException(Exception inner) : this(null, inner) { }
         public ModuleInitializationException(string message) : this(message, null) { }
         public ModuleInitializationException(string message, Exception inner) : base(message, inner) { }
+        protected ModuleInitializationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
