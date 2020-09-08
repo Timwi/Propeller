1e0badcbe27b30051b6d2e87b866e50e3c7a4671
Timwi <github@timwi.de>
2020-09-08 18:51:56 +0200

    Include RT.Servers, RT.Util and all the other RT libraries via Nuget, and add a nuspec for the PropellerApi

--- API/Properties/AssemblyInfo.cs
+++ API/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PropellerAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The client API required to write a module for the HTTP server Propeller.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Aldaray")]
+[assembly: AssemblyCompany("Timwi")]
 [assembly: AssemblyProduct("PropellerAPI")]
-[assembly: AssemblyCopyright("Copyright © Aldaray 2010")]
+[assembly: AssemblyCopyright("Copyright © Timwi 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9999.9999")]
-[assembly: AssemblyFileVersion("1.0.9999.9999")]
+[assembly: AssemblyVersion("1.1.9999.9999")]
+[assembly: AssemblyFileVersion("1.1.9999.9999")]
