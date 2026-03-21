42d9e2fb1bc2ff0c0d257e6e2889b3d2a9f0c1d3
Timwi <github@timwi.de>
2026-03-21 22:16:52 +0100

    Minor changes

--- FileServer/FileServer.cs
+++ FileServer/FileServer.cs
@@ -5,19 +5,17 @@ namespace RT.Propeller.Modules
 {
     public sealed class FileServer : PropellerModuleBase<FileServerSettings>
     {
-        public override string Name { get { return "FileServer"; } }
+        public override string Name => "FileServer";
 
         private FileSystemHandler Handler;
 
         public override void Init()
         {
-            Handler = new FileSystemHandler(Settings.Directory, Settings.Options);
+            Handler = Settings?.Directory == null ? null : new FileSystemHandler(Settings.Directory, Settings.Options);
         }
 
-        public override HttpResponse Handle(HttpRequest req)
-        {
-            return Handler.Handle(req);
-        }
+        public override HttpResponse Handle(HttpRequest req) =>
+            Handler == null ? HttpResponse.Html("<h1>Error</h1><p>The file server is not configured.</p>", HttpStatusCode._503_ServiceUnavailable) : Handler.Handle(req);
     }
 
     /// <summary>Contains the settings for the FileServer Propeller module.</summary>
